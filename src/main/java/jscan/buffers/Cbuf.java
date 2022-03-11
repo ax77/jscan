6122bae1026631843fdd541ffdfd69a555efca6a
ax77 <mailbox.r8@gmail.com>
2022-03-11 12:53:48 +0600

    change Cbuffer a little.

--- src/main/java/jscan/buffers/Cbuf.java
+++ src/main/java/jscan/buffers/Cbuf.java
@@ -3,7 +3,7 @@ package jscan.buffers;
 import static jscan.main.Env.HC_FEOF;
 
 public class Cbuf {
-  private static final int EOFS_PADDING_BUFLEN = 8;
+  private static final int EOFS_PADDING_BUFLEN = 16;
 
   private final char buffer[];
   private final int size;
@@ -14,20 +14,31 @@ public class Cbuf {
   private int eofs;
 
   public Cbuf(final String input) {
+    final StringBuilder sb = prepareInput(input);
 
+    this.buffer = sb.toString().toCharArray();
+    this.size = buffer.length;
+    this.offset = 0;
+    this.line = 1;
+    this.column = 0;
+    this.prevc = '\0';
+    this.eofs = -1;
+  }
+
+  private StringBuilder prepareInput(final String input) {
     final char[] content = input.toCharArray();
-    int offset = 0;
+    int from = 0;
 
     // Ignore the BOM, if any.
     if (content.length > 3) {
       if (content[0] == 0xef && content[1] == 0xbb && content[2] == 0xbf) {
-        offset = 3;
+        from = 3;
       }
     }
 
     // Build the main buffer
     final StringBuilder sb = new StringBuilder();
-    for (int i = offset; i < content.length; i += 1) {
+    for (int i = from; i < content.length; i += 1) {
       final char c = content[i];
       sb.append(c);
     }
@@ -37,12 +48,7 @@ public class Cbuf {
       sb.append("\0");
     }
 
-    this.buffer = sb.toString().toCharArray();
-    this.size = buffer.length;
-    this.line = 1;
-    this.column = 0;
-    this.prevc = '\0';
-    this.eofs = -1;
+    return sb;
   }
 
   public boolean isEof() {
@@ -152,14 +158,24 @@ public class Cbuf {
         throw new RuntimeException("Infinite loop handling...");
       }
 
+      if (offset >= size) {
+        eofs += 1;
+        return HC_FEOF; // '\0';
+      }
+
       if (prevc == '\n') {
         line += 1;
         column = 0;
       }
 
-      if (buffer[offset] == '\\') {
-        if (buffer[offset + 1] == '\r') {
-          if (buffer[offset + 2] == '\n') {
+      // we do not care about IOOB, because we ALWAYS have a little padding
+      char c1 = buffer[offset + 0];
+      char c2 = buffer[offset + 1];
+      char c3 = buffer[offset + 2];
+
+      if (c1 == '\\') {
+        if (c2 == '\r') {
+          if (c3 == '\n') {
             // DOS: [\][\r][\n]
             offset += 3;
           } else {
@@ -172,15 +188,15 @@ public class Cbuf {
         }
 
         // UNX: [\][\n]
-        if (buffer[offset + 1] == '\n') {
+        if (c2 == '\n') {
           offset += 2;
           prevc = '\n';
           continue;
         }
       }
 
-      if (buffer[offset] == '\r') {
-        if (buffer[offset + 1] == '\n') {
+      if (c1 == '\r') {
+        if (c2 == '\n') {
           // DOS: [\r][\n]
           offset += 2;
         } else {
@@ -191,15 +207,15 @@ public class Cbuf {
         return '\n';
       }
 
-      if (offset == size) {
-        eofs += 1;
-        return HC_FEOF; // '\0';
-      }
-
       char next = buffer[offset];
       offset += 1;
-      column += 1;
       prevc = next;
+
+      if (next == '\t') {
+        column += 4;
+      } else {
+        column += 1;
+      }
 
       if (next == '\0') {
         eofs += 1;
