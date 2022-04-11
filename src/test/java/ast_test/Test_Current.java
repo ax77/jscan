cf0bdc6943122201406472f4ec32720c16ac660a
ax77 <mailbox.r8@gmail.com>
2022-04-11 12:38:46 +0600

    add Main.java for future use. and fix maven deps.

---
+++ src/test/java/ast_test/Test_Current.java
@@ -0,0 +1,34 @@
+package ast_test;
+
+import java.io.IOException;
+
+import org.junit.Test;
+
+import ast.main.ParseMainNew;
+import ast.main.ParseOpts;
+import ast.tree.TranslationUnit;
+
+public class Test_Current {
+
+  private TranslationUnit parseUnit(StringBuilder sb) throws IOException {
+    // ParseOpts.CONCAT_STRINGS, ParseOpts.PREPEND_PREDEFINED_BUFFER
+    ParseOpts opts[] = new ParseOpts[] {};
+    TranslationUnit unit = new ParseMainNew(opts).parseString(sb.toString());
+    return unit;
+  }
+
+  @Test
+  public void test() throws IOException {
+    //@formatter:off
+    StringBuilder sb = new StringBuilder();
+    sb.append(" /*005*/  int f(int a, int b, int c) {        \n");
+    sb.append(" /*006*/   int d = 0;                         \n");
+    sb.append(" /*007*/   return a+b+c*d;                    \n");
+    sb.append(" /*008*/  }                                   \n");
+    //@formatter:on
+
+    TranslationUnit unit = parseUnit(sb);
+    System.out.println(unit);
+  }
+
+}
